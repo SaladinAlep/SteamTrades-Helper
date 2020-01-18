f2e9c54cd3f67cdb07dd6c636e734b9d934aaef4
Saladin Alep <saladinalep@outlook.com>
2020-01-18 22:06:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SteamTrades Helper
+Message notifications for SteamTrades.com
+
+This is a browser extension that provides the message notifications function of my [PySteamTrades](https://github.com/SaladinAlep/PySteamTrades) script.
+
+It runs in the background, checks the URL https://www.steamtrades.com/messages every 5 minutes and displays a desktop notification for new messages.
+
+You can add it to Firefox at:
+https://addons.mozilla.org/en-US/firefox/addon/steamtrades-helper/
+
+## Acknowledgments
+Icon made by [Freepik](https://www.flaticon.com/authors/freepik) from www.flaticon.com
---
+++ background.js
@@ -0,0 +1,35 @@
+const stUrl = 'https://www.steamtrades.com/messages';
+
+// permalinks of comments we already notified the user of
+let permalinks = [];
+
+chrome.alarms.onAlarm.addListener(function() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', stUrl);
+    xhr.responseType = 'document';
+    xhr.send();
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+            console.log(`Error opening page ${stUrl}`);
+            return;
+        }
+
+        let comments = xhr.response.getElementsByClassName("comment_inner");
+        for (i = 0; i < comments.length; ++i) {
+            let unread = comments[i].getElementsByClassName("comment_unread")
+            if (unread.length == 0)
+                // break on first read comment
+                break;
+            let links = comments[i].getElementsByTagName("a");
+            let permalink = links[links.length - 1].getAttribute('href');
+            if (permalinks.includes(permalink))
+                continue;
+            let author = comments[i].getElementsByClassName("author_name")[0].innerText;
+            let message = comments[i].getElementsByClassName("comment_body_default markdown")[0].innerText;
+            permalinks.push(permalink);
+            chrome.notifications.create('', {title: `New message from ${author}`, message: message, type: 'basic', iconUrl: 'images/icon64.png'});
+        }
+    }
+});
+
+chrome.alarms.create('STHelper', {when: Date.now(), periodInMinutes: 5});
